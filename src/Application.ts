c411b44b4191039eed7c56b316d4d0b8362b05fe
Jean-François Cloutier <ancientwinds@gmail.com>
2017-12-01 13:30:17 -0500

    Adding coveo.com...
    So we bind on the searchbox.

--- src/Application.ts
+++ src/Application.ts
@@ -37,6 +37,8 @@ export class Application extends BasicComponent {
             this.watchInput('[name="q"]');
         } else if (Url.checkIfUrlLocationContains('outlook.office.com')) {
             this.watchInput('[autoid="_is_3"]', true);
+        } else if (Url.checkIfUrlLocationContains('coveo.com')) {
+            this.watchInput('[form="coveo-dummy-form"]');
         } else if (Url.checkIfUrlLocationContains('html/popup.html')) {
             this._popup.render('body');
         } else if (!Url.checkIfUrlLocationContains('cloud.coveo.com/pages')) {
